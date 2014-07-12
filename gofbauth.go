654cd3faeb5e1d779ba3b5e77bbdfd1485e4a608
ernado <ernado@ya.ru>
2014-07-12 11:02:52 +0400

    init

---
+++ gofbauth.go
@@ -0,0 +1,159 @@
+package govkauth
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+const (
+	host                  = "www.facebook.com"
+	scheme                = "https"
+	accessTokenAction     = "oauth/access_token"
+	fbGraphHost           = "graph.facebook.com"
+	appIDParameter        = "client_id"
+	accessTokenParameter  = "access_token"
+	expiresParameter      = "expires"
+	appSecretParameter    = "client_secret"
+	version               = "5.23"
+	versionParameter      = "v"
+	responseTypeParameter = "response_type"
+	responseTypeCode      = "code"
+	redirectParameter     = "redirect_uri"
+	scopeParameter        = "scope"
+	authAction            = "dialog/oauth"
+	codeParameter         = "code"
+	usersGetAction        = "me"
+)
+
+var (
+	// ErrorBadCode occures when server returns blank code or error
+	ErrorBadCode = errors.New("bad code")
+	// ErrorBadResponse occures when server returns unexpected response
+	ErrorBadResponse                = errors.New("bad server response")
+	httpClient       mockHTTPClient = &http.Client{}
+)
+
+// Client for vkontakte oauth
+type Client struct {
+	ID          string
+	Secret      string
+	RedirectURL string
+	Scope       string
+}
+
+type User struct {
+	ID      int64 `json:"uid"`
+	Picture struct {
+		URL string `json:"url"`
+	}
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Gender    string `json:"gender"`
+	Email     string `json:"email"`
+}
+
+// AccessToken describes oath server response
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	Expires     int    `json:"expires"`
+}
+
+type mockHTTPClient interface {
+	Get(url string) (res *http.Response, err error)
+}
+
+func (client *Client) base(action string) url.URL {
+	u := url.URL{}
+	u.Host = host
+	u.Scheme = scheme
+	u.Path = action
+
+	query := u.Query()
+	query.Add(appIDParameter, client.ID)
+	query.Add(redirectParameter, client.RedirectURL)
+
+	u.RawQuery = query.Encode()
+	return u
+}
+
+// DialogURL is url for vk auth dialog
+func (client *Client) DialogURL() url.URL {
+	u := client.base(authAction)
+
+	query := u.Query()
+	query.Add(scopeParameter, client.Scope)
+	query.Add(responseTypeParameter, responseTypeCode)
+
+	u.RawQuery = query.Encode()
+	return u
+}
+
+func (client *Client) accessTokenURL(code string) url.URL {
+	u := client.base(accessTokenAction)
+	u.Host = fbGraphHost
+
+	query := u.Query()
+	query.Add(appSecretParameter, client.Secret)
+	query.Add(codeParameter, code)
+
+	u.RawQuery = query.Encode()
+	return u
+}
+
+// GetAccessToken is handler for redirect, gets and returns access token
+func (client *Client) GetAccessToken(req *http.Request) (token *AccessToken, err error) {
+	query := req.URL.Query()
+	code := query.Get(codeParameter)
+	if code == "" {
+		err = ErrorBadCode
+		return nil, err
+	}
+
+	requestURL := client.accessTokenURL(code)
+	res, err := httpClient.Get(requestURL.String())
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	q, err := url.ParseQuery(string(data))
+	if err != nil {
+		return
+	}
+	token = &AccessToken{}
+	token.AccessToken = q.Get(accessTokenParameter)
+	if token.AccessToken == "" {
+		err = ErrorBadResponse
+		return
+	}
+	token.Expires, err = strconv.Atoi(q.Get(expiresParameter))
+	return
+}
+
+func (client *Client) GetUser(uid int64) (user User, err error) {
+	u := client.base(usersGetAction)
+	u.Host = fbGraphHost
+	q := u.Query()
+	q.Del(appIDParameter)
+	q.Del(redirectParameter)
+	u.RawQuery = q.Encode()
+	res, err := httpClient.Get(u.String())
+	if err != nil {
+		return
+	}
+	decoder := json.NewDecoder(res.Body)
+	if err = decoder.Decode(&user); err != nil {
+		return
+	}
+	if user.Email == "" || user.FirstName == "" {
+		err = ErrorBadResponse
+		return
+	}
+	return user, nil
+}
---
+++ gofbauth_test.go
@@ -0,0 +1,114 @@
+package govkauth
+
+import (
+	"bytes"
+	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+type MockClient struct {
+	Response *http.Response
+}
+
+func (c *MockClient) Get(url string) (res *http.Response, err error) {
+	err = nil
+	if c.Response == nil {
+		err = http.ErrShortBody
+	}
+	return c.Response, err
+}
+
+func TestClient(t *testing.T) {
+	client := Client{"APP_ID", "APP_SECRET", "REDIRECT_URI", "PERMISSIONS"}
+	Convey("TestUrl", t, func() {
+		url := client.DialogURL()
+		should := "https://www.facebook.com/dialog/oauth?client_id=APP_ID&redirect_uri=REDIRECT_URI&response_type=code&scope=PERMISSIONS"
+		So(url.String(), ShouldEqual, should)
+	})
+
+	Convey("Test getName", t, func() {
+		res := &http.Response{}
+		body := `{
+		  "id": "1487207854850126", 
+		  "email": "ernado@ya.ru", 
+		  "first_name": "Alexander", 
+		  "gender": "male", 
+		  "last_name": "Razumov", 
+		  "link": "https://www.facebook.com/app_scoped_user_id/1487207854850126/", 
+		  "locale": "ru_RU", 
+		  "name": "Alexander Razumov", 
+		  "timezone": 4, 
+		  "updated_time": "2014-07-12T06:19:29+0000", 
+		  "verified": true
+		}`
+		res.Body = ioutil.NopCloser(bytes.NewBufferString(body))
+		httpClient = &MockClient{res}
+		user, err := client.GetUser(1)
+		So(err, ShouldBeNil)
+		So(user.FirstName, ShouldEqual, "Alexander")
+		So(user.LastName, ShouldEqual, "Razumov")
+		So(user.Email, ShouldEqual, "ernado@ya.ru")
+		So(user.Gender, ShouldEqual, "male")
+
+		Convey("Http error", func() {
+			httpClient = &MockClient{nil}
+			_, err := client.GetUser(1)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("json error", func() {
+			body := `[[[]}`
+			res.Body = ioutil.NopCloser(bytes.NewBufferString(body))
+			_, err := client.GetUser(1)
+			So(err, ShouldNotBeNil)
+		})
+		Convey("Server error", func() {
+			body := `{"response": {"error": "500"}}`
+			res.Body = ioutil.NopCloser(bytes.NewBufferString(body))
+			_, err := client.GetUser(1)
+			So(err, ShouldNotBeNil)
+		})
+	})
+
+	Convey("Test accessTokenUrl", t, func() {
+		Convey("Request url ok", func() {
+			urlStr := "https://graph.facebook.com/oauth/access_token?client_id=APP_ID&client_secret=APP_SECRET&code=CODE&redirect_uri=REDIRECT_URI"
+			url := client.accessTokenURL("CODE")
+			So(url.String(), ShouldEqual, urlStr)
+		})
+
+		urlStr := "http://REDIRECT_URI?code=7a6fa4dff77a228eeda56603b8f53806c883f011c40b72630bb50df056f6479e52a"
+		req, _ := http.NewRequest("GET", urlStr, nil)
+
+		resTok := &http.Response{}
+		body := `access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&expires=43200`
+		resTok.Body = ioutil.NopCloser(bytes.NewBufferString(body))
+		httpClient = &MockClient{resTok}
+
+		tok, err := client.GetAccessToken(req)
+		So(err, ShouldBeNil)
+		So(tok.AccessToken, ShouldEqual, "533bacf01e11f55b536a565b57531ac114461ae8736d6506a3")
+		So(tok.Expires, ShouldEqual, 43200)
+
+		Convey("Bad response", func() {
+			resTok.Body = ioutil.NopCloser(bytes.NewBufferString("asdfasdf"))
+			httpClient = &MockClient{resTok}
+			_, err := client.GetAccessToken(req)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Bad urk", func() {
+			req, _ = http.NewRequest("GET", "http://REDIRECT_URI?error=kek", nil)
+			_, err := client.GetAccessToken(req)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Http error", func() {
+			httpClient = &MockClient{nil}
+			_, err := client.GetAccessToken(req)
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
